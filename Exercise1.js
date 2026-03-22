faa59e0468dc712ddfb0e460ce50c556846d4802
Abuoma David <davidabuoma4@gmail.com>
2026-03-22 10:55:54 +0100

    initial commit

---
+++ Exercise1.js
@@ -0,0 +1,6 @@
+let favoriteColor = "blue";
+let favoriteFood = "jollof rice";
+let favoriteSport = "football";
+let favoriteMovie = "Interseller";
+
+console.log(`My favorite color is ${favoriteColor}, I love eating ${favoriteFood}, I enjoy ${favoriteSport}, and my favorite movie is ${favoriteMovie}.`)
