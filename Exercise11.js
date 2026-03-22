ce9e97e05f9d21bd0f48b095fe0909452b2f76d4
Abuoma David <davidabuoma4@gmail.com>
2026-03-22 11:56:22 +0100

    Exercise11

---
+++ Exercise11.js
@@ -0,0 +1,23 @@
+// String Builder
+
+let char = "*";
+let count = 6;
+let result = "";
+
+for (let i = 0; i < count; i++) {
+  result += char;
+}
+
+console.log(result);
+
+console.log("\nSecond run:");
+
+char = "-";
+count = 10;
+result = "";
+
+for (let i = 0; i < count; i++) {
+  result += char;
+}
+
+console.log(result);
