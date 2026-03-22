3fdc686b801b9ec354023cca757c65759712c40e
Abuoma David <davidabuoma4@gmail.com>
2026-03-22 11:14:21 +0100

    Exercise4

---
+++ Exercise4.js
@@ -0,0 +1,9 @@
+//  Circle Calculator
+
+let radius = 5;
+
+let area = Math.PI * radius * radius;
+let circumference = 2 * Math.PI * radius;
+
+console.log(`The area of the circle is ${area}`);
+console.log(`The circumference of the circle is ${circumference}`);
