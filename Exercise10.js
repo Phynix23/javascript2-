4339878355469bb0ad0e4e559a105cd54d24bebf
Abuoma David <davidabuoma4@gmail.com>
2026-03-22 11:49:08 +0100

    Exercise10

---
+++ Exercise10.js
@@ -0,0 +1,20 @@
+// Number to Words
+
+function numberToWords(num) {
+  if (num === 0) console.log(`Zero`);
+  else if (num === 1) console.log(`One`);
+  else if (num === 2) console.log(`Two`);
+  else if (num === 3) console.log(`Three`);
+  else if (num === 4) console.log(`Four`);
+  else if (num === 5) console.log(`Five`);
+  else if (num === 6) console.log(`Six`);
+  else if (num === 7) console.log(`Seven`);
+  else if (num === 8) console.log(`Eight`);
+  else if (num === 9) console.log(`Nine`);
+  else console.log(`Unknown`);
+}
+
+[0, 3, 7, 9, 11].forEach(num => {
+  console.log(`${num}:`);
+  numberToWords(num);
+});
