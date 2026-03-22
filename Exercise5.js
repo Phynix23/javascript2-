13f6ff72a242cef90f35b209395bb54dc9185f66
Abuoma David <davidabuoma4@gmail.com>
2026-03-22 11:19:18 +0100

    Exercise5

---
+++ Exercise5.js
@@ -0,0 +1,13 @@
+// Leap Year Checker
+
+function isLeapYear(year) {
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+      console.log(`${year} is a leap year.`);
+    } else {
+      console.log(`${year} is not a leap year.`);
+    }
+  }
+
+  [2000, 1900, 2023, 2100].forEach(year => {
+    isLeapYear(year);
+  });
